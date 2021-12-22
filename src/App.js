87a9f4d20bd21e893f4e643c765556db15371520
Sma-source <rabhi.smain@gmail.com>
2021-12-22 22:42:44 +0100

    add html structure

--- src/App.js
+++ src/App.js
@@ -2,6 +2,66 @@ function App() {
   return (
     <div className="App">
       <h1>Password Generator</h1>
+      <div className="container">
+        <div className="generator">
+          <h2 className="generator__header">Password Generator</h2>
+          <div className="generator__password">
+            <h3>Password</h3>
+            <button className="copy__btn">
+              <i className="far fa-clipboard"></i>
+            </button>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password-strength">Password length</label>
+            <input
+              type="number"
+              id="password-strength"
+              name="password-strength"
+              max="20"
+              min="10"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="uppercase-letters">Include Uppercase Letters</label>
+            <input
+              type="checkbox"
+              id="uppercase-letters"
+              name="uppercase-letters"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="lowercase-letters">Include Lowercase Letters</label>
+            <input
+              type="checkbox"
+              id="lowercase-letters"
+              name="lowercase-letters"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="include-numbers">Include Numbers</label>
+            <input
+              type="checkbox"
+              id="include-numbers"
+              name="include-numbers"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="include-symbols">Include Symbols</label>
+            <input
+              type="checkbox"
+              id="include-symbols"
+              name="include-symbols"
+            />
+          </div>
+
+          <button className="generator__btn">Generate Password</button>
+        </div>
+      </div>
     </div>
   );
 }
